bc7113b1d0fbc83dd02e2f14bd219d326570eba5
qiushui777 <xudong_shao@hotmail.com>
2019-08-19 23:05:22 +0800

    develop branch

--- working.py
+++ working.py
@@ -1,3 +1,4 @@
+"""
 import abupy
 from abupy import EMarketSourceType,EDataCacheType,EMarketTargetType,ABuSymbolPd,EMarketDataFetchMode,abu
 from abupy import ABuDateUtil
@@ -9,29 +10,28 @@ print(ABuSymbolPd.make_kl_df(symbol='QsETHUSDT'))
 
 
 """
+"""
 #binance api test
 import requests
 import abupy
 
-print(abupy.ABuDateUtil.datestr_unixm('2018-11-27'))
-print(abupy.ABuDateUtil.fmt_epoch(1543104000000))
+
 #print(requests.get(url='https://api.binance.com/api/v1/klines',params={"symbol":"BTCUSDT","interval": "1d", 
     #"startTime":1543104000000, "endTime":1543248000000}).text)
 print(requests.get(url='https://api.binance.com/api/v1/klines',params={"symbol":"BTCUSDT","interval": "1d"}).text)
-"""
+
 
 """
-huobi api test
+#huobi api test
 import websockets
 import asyncio
  
 async def hello():
     async with websockets.connect('wss://api.huobi.pro/ws') as websocket:
-        name = { "sub": "market.btccny.kline.1min",  "id": "id1"}
+        name = { "sub": "market.ethbtc.kline.1min",  "id": "id1"}
         await websocket.send(name)
         print(f"send server:{name}")
         greeting = await websocket.recv()
         print(f"receive from server:{greeting}")
 
 asyncio.get_event_loop().run_until_complete(hello())
-"""
